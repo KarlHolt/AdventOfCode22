d38ad01f813aebd2ccfcafc6514efa3b1a83dcb9
karlholt <karlimmanuelholt@gmail.com>
2022-12-01 11:05:23 +0100

    solution to day 1

--- Day1.py
+++ Day1.py
@@ -1,15 +1,20 @@
-def Solution1():
-    max_food = 0
-    with open("Day1input.txt", "r") as file:
+def insert_value(value, value_list):
+    last = value
+    for i in range(3):
+        if last > value_list[i]:
+            temp = value_list[i]
+            value_list[i] = last
+            last = temp
+
+max_food = [0,0,0]
+with open("Day1input.txt", "r") as file:
+    temp = 0
+    for line in file:
+        if(line.strip()):
+            temp += int(line)
+            continue
+        if temp > max_food[2]:
+            insert_value(temp, max_food)
         temp = 0
-        for line in file:
-            if(line.strip()):
-                temp += int(line)
-                continue
-            if temp > max_food:
-                max_food = temp
-            temp = 0
-    print(max_food)
-
-
-Solution1()
+    print("part 1:", max_food[0])
+    print("part 2:", sum(max_food))
